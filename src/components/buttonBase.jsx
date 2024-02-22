0be177938e9c09691acd5d325a237037f1bd85fc
Guillaume <robert.guillaume70@laposte.net>
2024-02-22 23:35:52 +0100

    add footer wave && fix some bugs

--- src/components/buttonBase.jsx
+++ src/components/buttonBase.jsx
@@ -1,4 +1,4 @@
-export default function Button({double, text, radius = "0px", type = "primary", classes = null, handleClick = ()=>  {}, selected = '', dataValue='', targetref})
+export default function Button({double, text, radius = "0px", type = "primary", classes = null, handleClick = ()=>  {}, dataValue='', targetref})
 {
 
         let classesStr = "";
@@ -7,14 +7,13 @@ export default function Button({double, text, radius = "0px", type = "primary", 
         {
          classesStr =  classes.join(' ')
         }
-   console.log(classesStr)
 
     if(double)
     {
         const textButtons = text.split(';')
         const dataValueText = text.split(';')
 
-        return <div className="group-btn"><button {...(dataValue != '' ? {"data-value": dataValueText[0]} : {})} onClick={ (e)=> handleClick(e) }  className={`btn double-btn btn-${type}`}>{textButtons[0]}</button><div className="separator"></div><button {...(dataValue != '' ? {"data-value": dataValueText[1]} : {})}  onClick={ (e)=> handleClick(e) } className={`btn double-btn btn-${type}`}>{textButtons[1]}</button></div>
+        return <div className="group-btn"><button {...(dataValue != '' ? {"data-value": dataValueText[0]} : {})} onClick={ (e)=> handleClick(e) }  className={`btn double-btn btn-${type}`}><span className="btn__text">{textButtons[0]}</span></button><div className="separator"></div><button {...(dataValue != '' ? {"data-value": dataValueText[1]} : {})}  onClick={ (e)=> handleClick(e) } className={`btn double-btn btn-${type}`}><span className="btn__text">{textButtons[1]}</span></button></div>
     }
-    return <button style={{'--radius': radius}} ref={targetref} {...(dataValue != '' ? {"data-value": dataValue} : {})}  onClick={ (e)=> handleClick(e) } className={`btn btn-${type} ${classesStr}`} >{text}</button>
+    return <button style={{'--radius': radius}} ref={targetref} {...(dataValue != '' ? {"data-value": dataValue} : {})}  onClick={ (e)=> handleClick(e) } className={`btn btn-${type} ${classesStr}`} ><span className="btn__text">{text}</span></button>
 }
